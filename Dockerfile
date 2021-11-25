50e069039d1d3fc305ca327d5a968453774f3cd1
Vipul Kumar <vipkumarsviit@gmail.com:wq>
2021-11-25 11:03:07 +0530

    Adds startup config to load data on application startup.

--- Dockerfile
+++ Dockerfile
@@ -1,5 +1,5 @@
 FROM openjdk:11-slim as build
 
-COPY target/account-0.0.1-SNAPSHOT.jar account-0.0.1-SNAPSHOT.jar
+COPY target/account-0.0.1-SNAPSHOT.jar app.jar
 
-ENTRYPOINT ["java", "-jar", "account-0.0.1-SNAPSHOT.jar"]
+ENTRYPOINT ["java", "-jar", "app.jar"]
--- src/main/java/com/awesomebank/account/AccountApplication.java
+++ src/main/java/com/awesomebank/account/AccountApplication.java
@@ -1,20 +1,14 @@
 package com.awesomebank.account;
 
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 @Slf4j
-public class AccountApplication implements CommandLineRunner {
+public class AccountApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(AccountApplication.class, args);
-    }
-
-    @Override
-    public void run(String... args) {
-        log.info("Application is started...");
     }
 }
